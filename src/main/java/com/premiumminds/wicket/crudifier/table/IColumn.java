706487254d8ce37d64988d12784cfe99fb66916b
André Camilo <andre.camilo@premium-minds.com>
2014-08-27 17:10:25 +0100

    code moved from pm-wicket-utils

---
+++ src/main/java/com/premiumminds/wicket/crudifier/table/IColumn.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (C) 2014 Premium Minds.
+ *
+ * This file is part of wicket-crudifier.
+ *
+ * wicket-crudifier is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * wicket-crudifier is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with wicket-crudifier. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.premiumminds.wicket.crudifier.table;
+
+import java.util.Map;
+
+import org.apache.wicket.Component;
+
+import com.premiumminds.wicket.crudifier.IObjectRenderer;
+
+public interface IColumn<T> {
+	public String getPropertyName();
+	public Component createComponent(String id, T object, Component resourceBase, Map<Class<?>, IObjectRenderer<?>> renderers); 
+	public ColumnAlign getAlign();
+}
